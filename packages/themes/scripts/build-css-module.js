baeba04549a15c7fed02c4a26c47c6004245028d
이윤슬 <ysisys0202@gmail.com>
2024-07-23 21:18:08 +0900

    ✨ color 변수 생성 기능 추가

---
+++ packages/themes/scripts/build-css-module.js
@@ -0,0 +1,59 @@
+import * as theme from "../dist/index.js";
+import fs from "fs";
+
+const toCssCasting = (str) => {
+  return str
+    .replace(/([a-z])(\d)/, "$1-$2")
+    .replace(/([A-Z])/g, "-$1")
+    .toLowerCase();
+};
+
+const generateThemeCssVariables = () => {
+  const cssString = [];
+  Object.entries(theme.vars).forEach(([key, value]) => {
+    if (key === "colors") {
+      Object.entries(value.$static).forEach(([colorKey, colorValue]) => {
+        if (colorKey === "light") {
+          const selector = ":root";
+          const cssVariables = Object.entries(colorValue)
+            .map(([mainKey, mainValue]) =>
+              Object.entries(mainValue)
+                .map(
+                  ([subKey, subValue]) =>
+                    `--${toCssCasting(mainKey)}-${toCssCasting(
+                      subKey
+                    )} : ${subValue};`
+                )
+                .join("\n")
+            )
+            .join("\n");
+          cssString.push(`${selector} {\n${cssVariables}}\n`);
+        }
+        if (colorKey === "dark") {
+          const selector = ":root .theme-dark";
+          const cssVariables = Object.entries(colorValue)
+            .map(([mainKey, mainValue]) =>
+              Object.entries(mainValue)
+                .map(
+                  ([subKey, subValue]) =>
+                    `--${toCssCasting(mainKey)}-${toCssCasting(
+                      subKey
+                    )} : ${subValue};`
+                )
+                .join("\n")
+            )
+            .join("\n");
+          cssString.push(`${selector} {\n${cssVariables}}\n`);
+        }
+      });
+    }
+  });
+  return cssString;
+};
+
+const generateThemeCss = () => {
+  const variables = generateThemeCssVariables();
+  fs.writeFileSync("dist/themes.css", [...variables].join("\n"));
+};
+
+generateThemeCss();
