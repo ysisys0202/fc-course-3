8ff3a2f6fef914e4580d25c5ff56322f9008d12b
이윤슬 <ysisys0202@gmail.com>
2024-07-23 22:13:52 +0900

    ✨ typography css 변수 및 class 생성 기능 추가

--- packages/themes/scripts/build-css-module.js
+++ packages/themes/scripts/build-css-module.js
@@ -28,14 +28,58 @@ const generateThemeCssVariables = () => {
           .join("\n");
         cssString.push(`${selector} {\n${cssVariables}}\n`);
       });
+      return;
     }
+    const selector = `:root`;
+    const cssVariables = Object.entries(value)
+      .map(([mainKey, mainValue]) =>
+        Object.entries(mainValue)
+          .map(
+            ([subKey, subValue]) =>
+              `--${toCssCasting(mainKey)}-${toCssCasting(
+                subKey
+              )} : ${subValue};`
+          )
+          .join("\n")
+      )
+      .join("\n");
+    cssString.push(`${selector} {\n${cssVariables}}\n`);
   });
+  return cssString;
+};
+
+const generateThemeCssClasses = () => {
+  const cssString = [];
+  Object.entries(theme.classes).forEach(([, value]) => {
+    const cssClasses = Object.entries(value)
+      .map(([mainKey, mainValue]) =>
+        Object.entries(mainValue)
+          .map(([subKey, subValue]) => {
+            const className = `.${toCssCasting(mainKey)}-${toCssCasting(
+              subKey
+            )}`;
+            const styleProperties = Object.entries(subValue)
+              .map(
+                ([styleKey, styleValue]) =>
+                  `${toCssCasting(styleKey)}: ${styleValue};`
+              )
+              .join("\n");
+
+            return `${className} {\n${styleProperties}}`;
+          })
+          .join("\n")
+      )
+      .join("\n");
+    cssString.push(cssClasses);
+  });
+
   return cssString;
 };
 
 const generateThemeCss = () => {
   const variables = generateThemeCssVariables();
-  fs.writeFileSync("dist/themes.css", [...variables].join("\n"));
+  const classes = generateThemeCssClasses();
+  fs.writeFileSync("dist/themes.css", [...variables, ...classes].join("\n"));
 };
 
 generateThemeCss();
