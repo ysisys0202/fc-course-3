378ee7dcc7aa039e8ee6a3aa4ccebe60505806fc
이윤슬 <ysisys0202@gmail.com>
2024-07-24 20:01:46 +0900

    ♻️ esbuild-config 관리용 패키지 추가

---
+++ packages/esbuild-config/README.md
@@ -0,0 +1,3 @@
+# esbuild-config
+
+esbuild-config를 공통적으로 관리하기 위한 패키지
--- packages/themes/build.js
+++ packages/themes/build.js
@@ -1,40 +1,4 @@
-import esbuild from "esbuild";
+import run from "@fastcampus/esbuild-config";
 import pkg from "./package-lock.json" assert { type: "json" };
 
-const dev = process.argv.includes("--dev");
-const minify = !dev;
-
-const watch = process.argv.includes("--watch");
-
-const external = Object.keys({
-  ...pkg.dependencies,
-  ...pkg.peerDependencies,
-});
-
-const baseConfig = {
-  entryPoints: ["src/index.ts"],
-  bundle: true,
-  minify,
-  sourcemap: true,
-  outdir: "dist",
-  target: "es2019",
-  watch,
-  external,
-};
-
-Promise.all([
-  esbuild.build({
-    ...baseConfig,
-    format: "esm",
-  }),
-  esbuild.build({
-    ...baseConfig,
-    format: "cjs",
-    outExtension: {
-      ".js": ".cjs",
-    },
-  }),
-]).catch(() => {
-  console.error("Build Failed");
-  ProcessingInstruction.exit(1);
-});
+run({ pkg });
