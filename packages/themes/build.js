30807b381977e63e42fa89ac278a84b965cad0b8
이윤슬 <ysisys0202@gmail.com>
2024-07-24 20:50:53 +0900

    🐛  package 빌드 에러 수정

--- packages/themes/build.js
+++ packages/themes/build.js
@@ -1,4 +1,4 @@
 import run from "@fastcampus/esbuild-config";
-import pkg from "./package-lock.json" assert { type: "json" };
+import pkg from "./package.json" assert { type: "json" };
 
 run({ pkg });
